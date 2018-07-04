2cab5f2de8fe022c2d63b42567a21a679624ffb7
chsw <christian.swahn@gmail.com>
2018-07-04 21:50:12 +0200

    Cleaned up received ir code parsing.

--- panasonic_heatpump.ino
+++ panasonic_heatpump.ino
@@ -412,66 +412,40 @@ void interuptHandler()
 //  detachInterrupt(digitalPinToInterrupt(IRpin));
 }
 
-bool initialSend = false;
-bool valuesRequested = false;
 void loop()
 {
-  if(!valuesRequested)
+  // Interrupt triggered - detected possible IR code
+  if(interuptTriggered)
   {
-    valuesRequested = true;
+    currentpulse=0;
+    byteCount=0;
+    Serial.println("Start listening for IR codes");
+    long beforePulses = millis();
+    int count = receivePulses();
+    long afterPulses = millis();
+    interuptTriggered = false;
 
-  }
-  if((millis()>10000) && initialSend == false){
-    initialSend=true;
-/*    send(msgStatus.set(current.status));
-    send(msgFan.set(current.fan));
-    send(msgSetpoint.set(current.setpoint,1));
-    send(msgMode.set(current.mode));
-    send(msgAirSwing.set(current.airSwing));
-    send(msgProfile.set(current.profile));
-*/    
-  }
+    Serial.print(F("  Received "));
+    Serial.print(count);
+    Serial.print(F(" bytes in "));
+    Serial.print(afterPulses - beforePulses);
+    Serial.println(F("ms."));
 
-    if(interuptTriggered)
+    if(decodePanasonicCS(irbytes, count))
     {
-        currentpulse=0;
-      byteCount=0;
-      Serial.println("Start listening for pulses");
-       // memset(symbols, 0, sizeof(symbols));
-      long beforePulses = millis();
-      
-      int count = receivePulses();
-      long afterPulses = millis();
-
-//      attachInterrupt(digitalPinToInterrupt(IRpin), interuptHandler, CHANGE);
-      interuptTriggered = false;
-
-      Serial.print(F("Pulses received: "));
-      Serial.print(afterPulses - beforePulses);
-      Serial.println(F("ms"));
-      Serial.print(F("Bytes received: "));
-      Serial.println(count);
-      
-//      printPulses();
-      if(count >= 27 && decodePanasonicCS(irbytes, count))
-      {
-      
       fetchValues(irbytes);
-
-
     }
-    
-    else
-     Serial.println(F("Expected 27 bytes, skipping parse"));
-    }
-    if(lastTransmitt < lastCommandReceived)
-    {
-      lastTransmitt = millis();
-      // Transmitt new values
-      sendHeatpumpIRCommand();
-      delay(10);
-      interuptTriggered = false; // Trigga inte på våra egna pulser
-    }
+  }
+  // If we received new command since last IR transmitt - transmitt model to heat pump
+  if(lastTransmitt < lastCommandReceived)
+  {
+    lastTransmitt = millis();
+    // Transmitt new values
+    sendHeatpumpIRCommand();
+    // Dont trigger on our own transmitt
+    delay(10); // This delay is needed as transmitted signals might be picked up by our receiver (sensor delay)
+    interuptTriggered = false; 
+  }
       // Send updates
       if(current.status_dirty)
       {
@@ -924,19 +898,29 @@ void verifyBuffer(bool was, bool expected, char *name)
     }
 }
 
+// Checks if this seems to be a valid ir code
 bool decodePanasonicCS(byte *bytes, int byteCount)
 {
-    // check if panasonic
-    if(byteCount != 27){
-      Serial.print(F("Received ir signal not 27 bytes! ("));
-      Serial.print(byteCount);
-      Serial.println(F("bytes)."));
-      Serial.println(F("Using last 27 bytes"));
-      bytes += byteCount-27;
-      //return false;
-    }
+  if(byteCount < 27)
+  {
+    Serial.println(F("Received less then 27 bytes over IR. Skipping parse."));
+    return false;
+  }
+  // check if panasonic
+  else if(byteCount > 27)
+  {
+    Serial.print(F("Received more then 27 bytes ("));
+    Serial.print(byteCount);
+    Serial.println(F("bytes) over IR. Parsing last 27 bytes as Panasonic CS:"));
+    bytes += byteCount-27;
+  }
+  else
+  {
+    Serial.println(F("Received 27 bytes over IR. Parsing as Panasonic CS:"));
+  }
 
-        // Print the byte array
+  // Print the byte array
+  Serial.print(F("  IR-byte array: "));
   for (int i = 0; i < 27; i++) {
     if (bytes[i] < 0x10) {
       Serial.print(F("0"));
@@ -947,204 +931,82 @@ bool decodePanasonicCS(byte *bytes, int byteCount)
     }
   }
   Serial.println(F(""));
-            // checksum algorithm
-        // Assumption: bitwise sum of payload bytes (so ignore header, and last byte)
-        byte checksum = 0x06; // found differce, so start with 0x06
-        //Serial.print(F("Add bytes: "));
-        for (int i = 13; i < 26; i++){
-            if (bytes[i] < 0x10) {
-          //    Serial.print(F("0"));
-            }
-            //Serial.print(bytes[i],HEX);
-            //Serial.print(F("+"));
-            checksum = checksum + bytes[i];
-        }
-//        Serial.println(F("."));
-  //      Serial.print(F("Checksum Truncate: "));
-        checksum = (checksum & 0xFF); // mask out only first byte)
-        Serial.print(F("Calculated checksum: "));
-        Serial.print(checksum,HEX);
-        if (checksum == bytes[26]){
-            Serial.println(F(". Checksum OK"));
-        }
-        else
-        {
-            Serial.print(F(". Checksum not OK. Should be: "));
-            Serial.println(bytes[26]);
-            return false;
-        }
-    // Test if looks like Pansonic code
-    // check via byteCount and fixed values
-    if (bytes[10 == 0xE0]){ // I just picked a value here
-        Serial.println(F("Look like a Panasonic CS protocol"));
 
-        // check on-off
-        // 13th byte from start, first byte with info
-        // this is not a toggle, but a state
-        if ((bytes[13] & 0x01) == 0x01) { // check if the right bit set 
-            Serial.println(F("Powering ON!"));
-        } else {
-            Serial.println(F("Powering OFF"));
-        }
-
-      
-        // check mode
-        bool maintenanceHeat = !(bytes[14] & 0x20);
-        
-        // if this byte is used for other things, first mask the correct bits
-        Serial.print(F("Mode: "));
-        if(maintenanceHeat)
-          Serial.println(F("Maintenance Heat"));
-        else
-          switch ((bytes[13]) & 0xF0){ // masks the first 4 bits 1111 0000
-            case 0x00:
-                Serial.println(F("Auto"));
-                break;
-            case 0x40:
-                Serial.println(F("Heat"));
-                break;
-            case 0x30:
-                Serial.println(F("Cool"));
-                break;
-            case 0x20:
-                Serial.println(F("Dry"));
-                break;
-            case 0x60: 
-                Serial.println(F("Fan"));
-                break;
-            default:
-                Serial.println(F("Error"));
-                break;
-          }
-        // check temp
-        Serial.print(F("Temperature: "));
-        byte temp = (bytes[14] & 0x1E);
-        byte halftemp = (bytes[14] & 0x01);
-        temp = temp >> 1;
-        //Serial.print(offset);Serial.print(temp);Serial.println(bytes[14]);
-        Serial.print((temp+(maintenanceHeat?0:16))); // masks the middle 5 bits: 0x1E = 0001 1110
-        if(halftemp > 0)
-          Serial.println(F(".5"));
-        else
-          Serial.println();
-
-        // check fanspeed
-        Serial.print(F("Fan speed: "));
-        switch (bytes[16] & 0xF0){ // 0xF0 = 1111 0000 eerste 4 bits
-            case 0xA0:
-                Serial.println(F("FAN AUTO"));
-                break;
-            case 0x30:
-                Serial.println(F("FAN 1"));
-                break;
-            case 0x40:
-                Serial.println(F("FAN 2"));
-                break;
-            case 0x50:
-                Serial.println(F("FAN 3"));
-                break;    
-            case 0x60:
-                Serial.println(F("FAN 4"));
-                break;    
-            case 0x70:
-                Serial.println(F("FAN 5"));
-                break;
-            default:
-                Serial.println(F("Error"));
-                break;
-        }
-
-        // check vertical swing
-        Serial.print(F("Vertical swing: "));
-        switch (bytes[16] & 0x0F){ // 0x0F = 0000 1111
-            case 0x0F:
-                Serial.println(F("AUTO"));
-                break;
-            case 0x01:
-                Serial.println(F("Straight"));
-                break;
-            case 0x02:
-                Serial.println(F("Down 1"));
-                break;
-            case 0x03:
-                Serial.println(F("Down 2"));
-                break;    
-            case 0x04:
-                Serial.println(F("Down 3"));
-                break;    
-            case 0x05:
-                Serial.println(F("Down 4"));
-                break;
-            default:
-                Serial.println(F("Error"));
-                break;
-        }
-
-        // check horizontal swing
-        Serial.print(F("Horizontal swing: "));
-        switch (bytes[17] & 0x0F){ // 0x0F = 0000 1111
-            case 0x0D:
-                Serial.println(F("AUTO"));
-                break;
-            case 0x06:
-                Serial.println(F("Center"));
-                break;
-            case 0x09:
-                Serial.println(F("Left"));
-                break;
-            case 0x0A:
-                Serial.println(F("Left center"));
-                break;    
-            case 0x0B:
-                Serial.println(F("Right center"));
-                break;    
-            case 0x0C:
-                Serial.println(F("Right"));
-                break;
-            default:
-                Serial.println(F("Error"));
-                break;
-        }
-        // Check profile
-        Serial.print(F("Profile: "));
-        switch(bytes[21]){
-           case 0x00:
-              Serial.println(F("Auto"));
-              break;
-           case 0x01:
-              Serial.println(F("Powerful"));
-              break;
-           case 0x20:
-              Serial.println(F("Quiet"));
-              break;
-           default:
-              Serial.println(F("Error"));
-        }
-
-        // timer A (start)
-        Serial.print(F("Timer A active: "));
-        if((bytes[13] & 0x02) == 0x02){ //check if second bit in byte is active
-            Serial.println(F("Yes"));
-        } else {
-            Serial.println(F("No"));
-        }        
-
-        // timer B (end)
-        Serial.print(F("Timer B active: "));
-        if((bytes[13] & 0x04) == 0x04){ //check if third bit in byte is active
-            Serial.println(F("Yes"));
-        } else {
-            Serial.println(F("No"));
-        }
-        // no need to investigate actual timings if you're going to automate :-)
-
-
-
-        // good end
-        return true;
-    }
-
+  // Check checksum
+  // Assumption: bitwise sum of payload bytes (so ignore header, and last byte)
+  byte checksum = 0x06; // found differce, so start with 0x06
+  for (int i = 13; i < 26; i++){
+    checksum = checksum + bytes[i];
+  }
+  checksum = (checksum & 0xFF); // mask out only first byte)
+  Serial.print(F("  Calculated checksum: "));
+  Serial.print(checksum,HEX);
+  if (checksum == bytes[26]){
+    Serial.println(F(". Checksum OK"));
+  }
+  else
+  {
+    Serial.print(F(". Checksum not OK. Should be: "));
+    Serial.println(bytes[26]);
     return false;
+  }
+  // Test if looks like Pansonic code
+  // check via byteCount and fixed values
+  if (bytes[10] != 0xE0)
+  { 
+    Serial.println(F("Doesn't look like Panasonic CS protocol. Skipping parse."));
+    return false;
+  }
+  // Print received values as human readable
+  printBuffer(bytes);
+  // Print extra info not displayed in above print
+
+  // check horizontal swing
+  Serial.print(F("  Horizontal swing: "));
+  switch (bytes[17] & 0x0F)
+  {
+    case PANASONIC_AIRCON2_HS_AUTO:
+      Serial.println(F("AUTO"));
+      break;
+    case PANASONIC_AIRCON2_HS_MIDDLE:
+      Serial.println(F("Center"));
+      break;
+    case PANASONIC_AIRCON2_HS_LEFT:
+      Serial.println(F("Left"));
+      break;
+    case PANASONIC_AIRCON2_HS_MLEFT:
+      Serial.println(F("Left center"));
+      break;    
+    case PANASONIC_AIRCON2_HS_MRIGHT:
+      Serial.println(F("Right center"));
+      break;    
+    case PANASONIC_AIRCON2_HS_RIGHT:
+      Serial.println(F("Right"));
+      break;
+    default:
+      Serial.print(F("UNDEFINED ("));
+      Serial.print(bytes[17]&0x0F);
+      Serial.println(F(")"));
+      break;
+  }
+
+  // timer A (start)
+  Serial.print(F("  Timer A active: "));
+  if((bytes[13] & 0x02) == 0x02){ //check if second bit in byte is active
+    Serial.println(F("Yes"));
+  } else {
+    Serial.println(F("No"));
+  }        
+
+  // timer B (end)
+  Serial.print(F("  Timer B active: "));
+  if((bytes[13] & 0x04) == 0x04){ //check if third bit in byte is active
+    Serial.println(F("Yes"));
+  } else {
+    Serial.println(F("No"));
+  }
+  // Parsing as Panasonic CS ok
+  return true;
 }
 
 void fetchValues(byte *bytes)
